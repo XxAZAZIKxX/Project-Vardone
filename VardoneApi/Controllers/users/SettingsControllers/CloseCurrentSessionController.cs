d2cbe9258da457ede3a1f0be6d1ceefb22ce0db6
Sasha Semachkin <az195753@gmail.com>
2021-03-11 00:09:28 +0200

    Добавлена возможность завершать сесию или все сесии

---
+++ VardoneApi/Controllers/users/SettingsControllers/CloseCurrentSessionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VardoneEntities.Models.GeneralModels.Users;
+
+namespace VardoneApi.Controllers.users.SettingsControllers
+{
+    [ApiController, Route("users/[controller]")]
+    public class CloseCurrentSessionController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Post([FromHeader] long userId, [FromHeader] string token)
+        {
+            return Task.Run(new Func<IActionResult>(() =>
+            {
+                if (string.IsNullOrWhiteSpace(token)) return BadRequest("Empty token");
+                if (!Core.UserChecks.CheckToken(new UserTokenModel { UserId = userId, Token = token }))
+                    return Unauthorized("Invalid token");
+                var tokens = Program.DataContext.Tokens;
+                tokens.Include(p => p.User).Load();
+
+                try
+                {
+                    var first = tokens.First(p => p.User.Id == userId && p.Token == token);
+                    tokens.Remove(first);
+                    Program.DataContext.SaveChanges();
+                    return Ok();
+                }
+                catch
+                {
+                    return BadRequest();
+                }
+            })).GetAwaiter().GetResult();
+        }
+    }
+}
