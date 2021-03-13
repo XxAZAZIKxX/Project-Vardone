2a1a30c173011a6443e2ca78c1364e8338d88551
Sasha Semachkin <az195753@gmail.com>
2021-03-13 18:35:59 +0200

    Revert "Revert "null""
    This reverts commit c8389862fca899cd36263205cc6ca300db10f870.

---
+++ VardoneApi/Entity/Models/UsersOnlineTable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VardoneApi.Entity.Models
+{
+    [Table("users_online")]
+    public class UsersOnlineTable
+    {
+        [Column("id"), Key] public long Id { get; set; }
+        [Column("user_id"), ForeignKey("user_id"), Required] public virtual UsersTable User { get; set; }
+        [Column("last_online"), Required] public DateTime LastOnlineTime { get; set; }
+    }
+}
